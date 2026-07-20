f5f7ac8b11b59ce613df52f4bae3138334836ed8
graceanax <graceanax@gmail.com>
2026-07-20 16:53:37 -0600

    Se agrega fecha como un solo entero

--- MyDate/Main.java
+++ MyDate/Main.java
@@ -1,11 +1,16 @@
 // Ana Pérez
 public class Main {    
     public static void main(String[] args) {
+        long fecha = 22081999L;
+        int anio = (int) (fecha%10000);
+        int mes = (int) ((fecha/10000)%100);
+        int day = (int) (fecha/1000000);
+
         MyDate d = new MyDate();
-        // prueba 1
-        d.setYear(1901);
-        d.setMonth(2);
-        d.setDay(32);
+
+        d.setYear(anio);
+        d.setMonth(mes);
+        d.setDay(day);
 
         System.out.println(d.getDay() + "-" + d.getMonth() + "-" + d.getYear());
     }
